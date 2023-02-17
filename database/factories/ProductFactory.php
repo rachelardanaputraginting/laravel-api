93e16cd7521ee298df8293c5c5441924d9ee4352
rachelardanaputraginting <rachelardanaputraginting@gmail.com>
2023-02-17 23:10:06 +0700

    Belajar API, membuat schema database

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'category_id' => Category::factory(),
+            'name' => $name = $this->faker->sentence(),
+            'slug' => Str::slug($name),
+            'description' => $this->faker->paragraph(25),
+            'price' => rand(111111, 999999)
+        ];
+    }
+}
