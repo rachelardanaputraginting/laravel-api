e70267a17e5b79c230f5b6fbd2720fa51672024e
rachelardanaputraginting <rachelardanaputraginting@gmail.com>
2023-02-18 14:43:58 +0700

    Membuat Route, Controller dan Api Request

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,6 +9,11 @@ class Product extends Model
 {
     use HasFactory;
 
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class);
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-
+Route::apiResource('products', ProductController::class);
