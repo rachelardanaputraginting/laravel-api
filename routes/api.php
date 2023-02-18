a3900a39e0ed34494e6920bb8d362ab3473bbe72
rachelardanaputraginting <rachelardanaputraginting@gmail.com>
2023-02-18 13:05:52 +0700

    Tes route Postman

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,4 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('first-name', fn () => response()->json([
-    "name" => "Rachel"
-]));
 
-Route::get('last-name', fn () => response()->json([
-    "name" => "Ginting"
-]));
